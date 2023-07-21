64c1f95d860b7e4f4cb0d9392c4c9e13b312fe45
vidit <agrawalvs_3@rknec.edu>
2023-07-22 00:03:36 +0530

    welcome

---
+++ add.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+int a,b;
+printf("enter 1 st  number\n");
+scanf("%d",&a);  
+printf("enter 2 nd number\n");
+scanf("%d",&b);
+printf("sum is %d \n",a+b);
+return 0;
+}
+
+
